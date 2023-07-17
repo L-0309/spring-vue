e6a1977cd675e2a2ae8b1c3eab94d45d15248029
LiuHaoYu <liu030910@gmail.com>
2023-07-17 14:10:12 +0800

    修改前台图片

--- src/views/front/ItemPicture.vue
+++ src/views/front/ItemPicture.vue
@@ -4,17 +4,17 @@
     <h3 style="color: red">因为此处图片是后添加的，此处的字段和库中的字段发生了冲突，此处仅显示两张</h3>
     <vue-picture-swipe :items="items"></vue-picture-swipe>
 
-    <el-row :gutter="5">
-      <el-col :span="4" v-for="(item,index) in files" :key="item.id"
-              style="height: 300px;border-bottom: 1px solid #ccc">
-        <el-card>
-          <div style="height: 200px">
-            <img :src="item.url" alt="logo" style="width: 100%;"/>
-            <span style="color: #666">{{ item.name.substring(0, item.name.indexOf('.')) }}</span>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
+<!--    <el-row :gutter="5">-->
+<!--      <el-col :span="4" v-for="(item,index) in files" :key="item.id"-->
+<!--              style="height: 300px;border-bottom: 1px solid #ccc">-->
+<!--        <el-card>-->
+<!--          <div style="height: 200px">-->
+<!--            <img :src="item.url" alt="logo" style="width: 100%;"/>-->
+<!--            <span style="color: #666">{{ item.name.substring(0, item.name.indexOf('.')) }}</span>-->
+<!--          </div>-->
+<!--        </el-card>-->
+<!--      </el-col>-->
+<!--    </el-row>-->
   </div>
 </template>
 
@@ -30,30 +30,30 @@ export default {
       items: [
         {
           //点击打开时候的图片
-          src: 'http://' + serverIp + ':8111/images/20201282863486.jpg',
+          src: '',
           //页面显示的土图片
-          thumbnail: 'http://' + serverIp + ':8111/images/20201282863486.jpg',
+          thumbnail: '',
           //打开图片时的宽和高
-          w: 600,
-          h: 400,
+          w: '',
+          h: '',
           //打开后的标题
-          title: '管理员专用头像'
+          title: ''
         },
-        {
-          src: 'http://' + serverIp + ':8111/images/OIP-C.jpg',
-          thumbnail: 'http://' + serverIp + ':8111/images/OIP-C.jpg',
-          w: 600,
-          h: 400,
-          title: 'ikun'
-        }
+        // {
+        //   src: 'http://' + serverIp + ':8111/images/OIP-C.jpg',
+        //   thumbnail: 'http://' + serverIp + ':8111/images/OIP-C.jpg',
+        //   w: 600,
+        //   h: 400,
+        //   title: 'ikun'
+        // }
       ]
     }
   },
   methods: {
     load() {
-      request.get('files/items').then(
+      request.get('files/items/picture').then(
           res => {
-            this.files = res.data.filter(v => v.type === 'jpg' || v.type === 'png')
+            this.items = res.data.filter(v => v.type === 'jpg' || v.type === 'png')
           }
       )
     }
